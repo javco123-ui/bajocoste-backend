16710fc4de55fd429ece9ed3b001920662972120
javco123-ui <javco123@gmail.com>
2026-05-09 19:35:18 -0400

    Update index.py

--- prompts/index.py
+++ prompts/index.py
@@ -1,1 +1,10 @@
+from .suscripciones import PROMPT as SUSCRIPCIONES
+from .energia import PROMPT as ENERGIA
+from .deudas import PROMPT as DEUDAS
+
+PROMPTS = {
+    "suscripciones": SUSCRIPCIONES,
+    "energia": ENERGIA,
+    "deudas": DEUDAS
+}
 
